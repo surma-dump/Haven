21f81f3d3b831136a8e98cf213ba33e8762e71e4
GlassOnTin <GlassOnTin@users.noreply.github.com>
2026-03-04 00:20:50 +0000

    Bump to v0.8.6 and update F-Droid metadata

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -15,8 +15,8 @@ android {
         applicationId = "sh.haven.app"
         minSdk = 26
         targetSdk = 35
-        versionCode = 23
-        versionName = "0.8.5"
+        versionCode = 24
+        versionName = "0.8.6"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
 
