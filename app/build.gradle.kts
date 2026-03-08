9d375c4ee63eacad26149b9f5b86b8dcf47dcee8
GlassOnTin <GlassOnTin@users.noreply.github.com>
2026-03-08 01:01:25 +0000

    Bump to v1.0.17

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -15,8 +15,8 @@ android {
         applicationId = "sh.haven.app"
         minSdk = 26
         targetSdk = 35
-        versionCode = 43
-        versionName = "1.0.16"
+        versionCode = 44
+        versionName = "1.0.17"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
 
